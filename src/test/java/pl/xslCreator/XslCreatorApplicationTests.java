d824296509827cc49a9da653674671d4f66ccc74
worscik <58079969+worscik@users.noreply.github.com>
2023-03-06 21:32:00 +0100

    Create project
    Create project

---
+++ src/test/java/pl/xslCreator/XslCreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.xslCreator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XslCreatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
