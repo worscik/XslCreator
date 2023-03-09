c9a76fc2ea73bcec0487dfe1f5f2983ccb3d92a4
worsciko <kaczmarskim95@gmail.com>
2023-03-09 22:41:17 +0100

    Add api json file

--- src/main/java/pl/xslCreator/WebController/HomePage.java
+++ src/main/java/pl/xslCreator/WebController/HomePage.java
@@ -2,6 +2,7 @@ package pl.xslCreator.WebController;
 
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
